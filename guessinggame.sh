566399024ecdbbc1d034ee2e50947cdde80b48cc
satish-thuwal <satishthuwal@protonmail.com>
2020-04-19 15:18:10 +0530

    Created function to check fix number of filecounts

--- guessinggame.sh
+++ guessinggame.sh
@@ -2,10 +2,26 @@
 
 echo " We are going to play a game"
 echo " you will have to guess how many files are in this directory"
-echo "Don't worry, i will give you hint after every input and you will be allowed  infinite number of inouts"
+echo " Don't worry, i will give you hint after every input and you will be allowed  infinite number of inouts"
 
-echo "please enter initial guess and press enter"
+echo ">> please input initial guess and press enter"
 
-local read initial
+read input
 
+function check_input {
+	while [[ $input -ne 3 ]]
+	do
+		if [[ $input -gt 3 ]]
+		then echo ">>>>> Your input is too high, please enter input again"
+		read input
+		else
+		echo ">>>>> Your input is too low, please enter input again"
+		read input
+		fi
+	done
+	echo "You are right, there are 3 files in this directory, well done"
+			}
+check_input
+
+echo "The end!!"
 
