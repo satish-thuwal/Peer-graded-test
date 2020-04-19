679d8836d8e26fa1eae1d853b62d6a7000a1d46f
satish-thuwal <satishthuwal@protonmail.com>
2020-04-19 14:55:59 +0530

    First commit. adding the file

---
+++ guessinggame.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo " We are going to play a game"
+echo " you will have to guess how many files are in this directory"
+echo "Don't worry, i will give you hint after every input and you will be allowed  infinite number of inouts"
+
+echo "please enter initial guess and press enter"
+
+local read initial
+
+
